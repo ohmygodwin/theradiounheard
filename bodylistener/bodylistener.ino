288eebced05e468b905fff4ba56e0f956f3c1d9c
Kate Godwin <mkategodwin@gatech.edu>
2015-03-31 23:40:28 -0400

    radiosilence

--- bodylistener/bodylistener.ino
+++ bodylistener/bodylistener.ino
@@ -3,13 +3,14 @@ void setup() {
   Serial.begin(9600); //set baud rate
   //set pins attached to LEDs as outputs
   pinMode(6,OUTPUT);
+}
 
 void loop() {
   electret1 = analogRead(5); //set VU as value of pin A5 reading
 
   if (electret1 > 300) {
-    digitalWrite(6 , HIGH)
+    digitalWrite(6 , HIGH);
   } else {
-    digitalWrite(6 , LOW)
+    digitalWrite(6 , LOW);
   }
 }
